e4011d459ed7199ba02258ce999652d2ef3b2692
Hiren Gelani <hiren.gelani@netgear.com>
2024-08-02 15:30:15 +0530

    Initial Commit

---
+++ RecipeBrowser/RecipeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserApp.swift
+//  RecipeBrowser
+//
+//  Created by Smit Kanojiya on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
